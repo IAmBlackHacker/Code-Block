5291282faec13f6f8618752a76194082f57929b8
Lokesh Jagdish Bhoyar <lokesh.prenoramance@gmail.com>
2017-02-25 02:14:41 +0530

    Add files via upload

---
+++ Graphics in CodeBlocks/Test Graphics.h/Test Graphics.cpp
@@ -0,0 +1,30 @@
+#include<graphics.h>
+#include<conio.h>
+#include<dos.h>
+
+main()
+{
+   int gd = DETECT, gm, x, y, color, angle = 0;
+   struct arccoordstype a, b;
+   initgraph(&gd, &gm, "C:\\TC\\BGI");
+   delay(20);
+   settextstyle(6, HORIZ_DIR,5);
+   outtextxy(x, y, "Text with different fonts");
+   while(angle<=360)
+   {
+      setcolor(BLACK);
+      arc(getmaxx()/2,getmaxy()/2,angle,angle+2,100);
+      setcolor(RED);
+      getarccoords(&a);
+      circle(a.xstart,a.ystart,25);
+      setcolor(BLACK);
+      arc(getmaxx()/2,getmaxy()/2,angle,angle+2,150);
+      getarccoords(&a);
+      setcolor(GREEN);
+      circle(a.xstart,a.ystart,25);
+      angle = angle+5;
+      delay(50);
+   }
+   getch();
+   closegraph();
+}
